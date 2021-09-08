34bdb39c860d040180ef37618402578f8df52ac4
juancdri <juancdri@gmail.com>
2021-09-08 02:04:35 -0300

    socket.io connected

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const {DB_PASSWORD, DB_HOST, DB_NAME, DB_USER} = process.env;
 const {server} = require("./src/server");
 
+const app = require('http').createServer(server)
+
+const io = require('socket.io')(app, {cors:{origin: "*"}})
+
 mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
     {
@@ -11,7 +15,7 @@ mongoose.connect(
     }
 )
 .then((data)=>{
-    server.listen("3001", () => console.log("Listen in port 3001."));
+    app.listen("3001", () => console.log("Listen in port 3001."));
     console.log("Conexión exitosa a DB " + data.connections[0].name);
 })
 .catch(err => console.error(err))
