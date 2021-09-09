f2846160a2ade29fa51656d61c4e38497244e736
juancdri <juancdri@gmail.com>
2021-09-09 06:14:31 -0300

    app

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,12 @@ import { Route } from 'react-router-dom'
 import { Fragment } from 'react';
 import  Home from './components/Home'
 import Sensors from './components/Sensors/Sensors';
-import Nav from './components/Nav'
 
 
 function App() {
   return (
     <Fragment>
       <Route exact path='/' component={Home} />
-      {/* <Route path='/sensor' component={Nav} /> */}
       <Route exact path='/sensor' component={Sensors} />
     </Fragment>
   );
